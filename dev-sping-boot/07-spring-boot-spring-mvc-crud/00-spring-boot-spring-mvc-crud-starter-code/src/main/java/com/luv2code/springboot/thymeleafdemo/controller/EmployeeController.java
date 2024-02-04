c0f370a807b97d3c885c9dce50195659e8cff410
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-02-04 18:16:45 -0500

    crud + thymeleaf
    update and delete

--- dev-sping-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-sping-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,8 +4,7 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +28,52 @@ public class EmployeeController {
         //add to the spring model
         theModel.addAttribute("employees", theEmployee);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+
+        //create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        //save the employee
+        employeeService.save(theEmployee);
+
+        //use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+
+        //get the employee from the service
+        Employee theEmployee= employeeService.findById(theId);
+
+        //set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        //send over to our form
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+
+        //delete the employee
+        employeeService.deleteById(theId);
+
+        //redirect to the /employee/list
+        return "redirect:/employees/list";
+    }
+
+
 }
--- dev-sping-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ dev-sping-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,8 +3,12 @@ package com.luv2code.springboot.thymeleafdemo.dao;
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
 
+    //add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
