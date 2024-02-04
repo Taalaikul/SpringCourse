8936815f0b78b926b813b8aa32b1c13140551c44
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-02-03 20:44:23 -0500

    Validation form
    string trim
    required fields
    number validation

---
+++ dev-sping-boot/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -0,0 +1,43 @@
+package com.luv2code.springdemo.mvc;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Customer {
+
+    private String firstName;
+    @NotNull(message="is required")
+    @Size(min=1, message="is required")
+    private String lastName = "";
+
+    @Min(value=0, message="must be more than or equal to zero")
+    @Max(value=10, message="must be less than or equal to ten")
+    private int freePasses;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+}
