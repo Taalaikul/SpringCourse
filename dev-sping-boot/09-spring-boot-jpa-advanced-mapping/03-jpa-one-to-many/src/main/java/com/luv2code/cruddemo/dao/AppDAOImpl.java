911840bec749cb38790c1b796e0acf4bb5b01edc
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-02-06 12:36:31 -0500

    one to many relationship

---
+++ dev-sping-boot/09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,62 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    //define field for entity manager
+    private EntityManager entityManager;
+
+    //inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        //retrieve the instructor
+        Instructor theInstructor = entityManager.find(Instructor.class, theId);
+
+        //delete the instructor
+        entityManager.remove(theInstructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        //remove the associated object reference
+        //break bi-directional link
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(tempInstructorDetail);
+    }
+}
