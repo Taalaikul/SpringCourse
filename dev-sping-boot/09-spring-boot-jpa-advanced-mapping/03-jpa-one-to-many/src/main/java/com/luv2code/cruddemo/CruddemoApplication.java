c7e5f6f2359a084fb1bc49cbe0dd551ec4646acc
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-02-06 13:06:00 -0500

    one to many
    courses entity, create method for courses with instructors

--- dev-sping-boot/09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ dev-sping-boot/09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,6 +1,7 @@
 package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Course;
 import com.luv2code.cruddemo.entity.Instructor;
 import com.luv2code.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
@@ -23,8 +24,34 @@ public class CruddemoApplication {
 			//findInstructor(appDAO);
 			//deleteInstructor(appDAO);
 			//findInstructorDetail(appDAO);
-			deleteInstructorDetail(appDAO);
+			//deleteInstructorDetail(appDAO);
+			createInstructorWithCourses(appDAO);
 		};
+	}
+
+	private void createInstructorWithCourses(AppDAO appDAO) {
+
+		Instructor tempInstructor = new Instructor("Susan", "Public", "susan@luvtocode.com");
+
+		//create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.youtube.com", "Video Games");
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//create some courses
+		Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+		Course tempCourse2 = new Course("The Pinball Masterclass");
+
+		//add courses to instructor
+		tempInstructor.add(tempCourse1);
+		tempInstructor.add(tempCourse2);
+
+		//save the instructor
+		System.out.println("Saving instructor: " + tempInstructor );
+		System.out.println("The courses: "+ tempInstructor.getCourses());
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
 	}
 
 	private void deleteInstructorDetail(AppDAO appDAO) {
--- dev-sping-boot/09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
+++ dev-sping-boot/09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="instructor")
 public class Instructor{
@@ -28,6 +31,10 @@ public class Instructor{
     @OneToOne(cascade=CascadeType.ALL)
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor",
+            cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Course> courses;
 
     //create constructors
     public Instructor(){
@@ -80,6 +87,26 @@ public class Instructor{
 
     public void setInstructorDetail(InstructorDetail instructorDetail) {
         this.instructorDetail = instructorDetail;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    //add convenience method for bi-directional relationship
+    public void add(Course tempCourse){
+
+        if(courses==null){
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
     }
 
 
