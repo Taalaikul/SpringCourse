ab97ffc095709ea0a20428d16dddf5b1b48e5269
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-02-06 11:23:05 -0500

    dao, entity
    command line app

---
+++ dev-sping-boot/09-spring-boot-jpa-advanced-mapping/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,42 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    //define field for entity manager
+    private EntityManager entityManager;
+
+    //inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        //retrieve the instructor
+        Instructor theInstructor = entityManager.find(Instructor.class, theId);
+
+        //delete the instructor
+        entityManager.remove(theInstructor);
+    }
+}
