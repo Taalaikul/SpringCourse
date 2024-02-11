910aaa019c2d08bca6f15f1294ce933c4b453557
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-02-10 20:39:01 -0500

    JointPoints

--- dev-sping-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aop/demo/AopDemoApplication.java
+++ dev-sping-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aop/demo/AopDemoApplication.java
@@ -26,6 +26,8 @@ public class AopDemoApplication {
 
 		//call the business method
 		Account myAccount=new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
 
--- dev-sping-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aop/demo/aspect/MyDemoLoggingAspect.java
+++ dev-sping-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aop/demo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,11 @@
 package com.luv2code.aop.demo.aspect;
 
+import com.luv2code.aop.demo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +15,30 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.luv2code.aop.demo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(){
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
         System.out.println("\n====>>>> Executing @Before advice on method");
+
+        //display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: "+methodSignature);
+
+        //display method arguments
+
+        //get args
+        Object[] args = theJoinPoint.getArgs();
+
+        //loop thru args
+        for(Object tempArg : args){
+            System.out.println(tempArg);
+
+            if(tempArg instanceof Account){
+                //downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+                System.out.println("account name: "+theAccount.getName());
+                System.out.println("account level: "+theAccount.getLevel());
+            }
+        }
     }
 
 }
